8ea3c9b30a837a6b0a62178c9b648aa765b95fe7
batoolalshareef <batoolalshareef@gmail.com>
2022-11-28 04:01:40 +0200

    update

--- src/testcountlinks/CountLinks.java
+++ src/testcountlinks/CountLinks.java
@@ -1,10 +1,62 @@
 package testcountlinks;
 
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class CountLinks {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//test
+	public static void main(String[] args) throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver", "E:\\chromedriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com");
+		// List myList = driver.findElements(By.tagName("a")).size();
+		System.out.println(driver.findElements(By.tagName("a")).size());
+
+		WebElement myFooter = driver.findElement(By.id("pageFooter"));
+		System.out.println(myFooter.findElements(By.tagName("a")).size());
+
+		WebElement lastFooter = driver.findElement(By.id("pageFooterChildren"));
+		System.out.println(lastFooter.findElements(By.tagName("a")).size());
+
+//		for (int i=0; i<lastFooter.findElements(By.tagName("a")).size();i++) {
+//			
+//lastFooter.findElements(By.tagName("a")).get(i).click();
+//			Thread.sleep(2000);
+//		}	
+
+		for (int i = 0; i < lastFooter.findElements(By.tagName("a")).size(); i++) {
+
+			Thread.sleep(300);
+			String openLinks = Keys.chord(Keys.CONTROL, Keys.ENTER);
+			lastFooter.findElements(By.tagName("a")).get(i).sendKeys(openLinks);
+		}
+
+//		Set<String> opentab=driver.getWindowHandles();
+//		Iterator<String> tabview=opentab.iterator();
+//		
+//		while (tabview.hasNext()) {
+//			driver.switchTo().window(tabview.next());
+//			System.out.println(driver.getTitle());
+//			
+//		}
+		
+		Set<String> opentab=driver.getWindowHandles();
+		Iterator<String> tabview=opentab.iterator();
+		
+		while (tabview.hasNext()) {
+			driver.switchTo().window(tabview.next());
+			System.out.println(driver.getTitle());
+			
+		}
+		
 	}
 
 }
